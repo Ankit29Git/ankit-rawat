e45bc3be20dfc6772c24166dce7ef712412ef0f1
ankit.r <ankit.rawat@symboinsurance.com>
2021-03-29 19:28:36 +0530

    script change 1

--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ require('./server');
 
 
 
-// https://medium.com/free-code-camp/how-to-deploy-a-react-app-with-an-express-server-on-heroku-32244fe5a250
+//  https://medium.com/free-code-camp/how-to-deploy-a-react-app-with-an-express-server-on-heroku-32244fe5a250
+//  Used this link for this app its heroku deployment
 
-// Used this link for this app its heroku deployment
+//  https://stackoverflow.com/questions/13011881/node-js-simple-app-not-working-on-heroku/13013189
+// faced an issue as npm start was only starting server and not client(thus missing the build folder)
